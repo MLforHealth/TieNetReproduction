5389fad88e7d4630bb69bfd7f004ba157dce8260
Guanxiong Liu <guanxiong.liu@mail.utoronto.ca>
2018-11-10 20:59:46 -0500

    update

--- create_input_files.py
+++ create_input_files.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='mimiccxr',
                        base_path='/crimea/mimic-cxr/',
-                       reports_per_image=1,
+                       captions_per_image=1,
                        min_word_freq=5,
                        output_folder='/crimea/liuguanx/mimic-output/',
                        max_len=100)
