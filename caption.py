586f8537ffefef0e330d64dc6568faca48484909
Guanxiong Liu <guanxiong.liu@mail.utoronto.ca>
2019-01-23 09:22:44 -0500

    update

--- caption.py
+++ caption.py
@@ -196,7 +196,7 @@ def visualize_att(image_path, seq, alphas, rev_word_map, smooth=True):
 if __name__ == '__main__':
 
     # Load model
-    checkpoint = torch.load('/data/medg/misc/liuguanx/TieNetReproduction/BEST_5checkpoint_mimiccxr_1_cap_per_img_5_min_word_freq.pth.tar')
+    checkpoint = torch.load('/data/medg/misc/liuguanx/TieNetReproduction/BEST_15checkpoint_mimiccxr_1_cap_per_img_5_min_word_freq.pth.tar')
     decoder = checkpoint['decoder']
     decoder = decoder.to(device)
     decoder.eval()
@@ -213,20 +213,19 @@ if __name__ == '__main__':
     test_data = pd.read_csv('/data/medg/misc/liuguanx/dataset/val.csv')
     text = []
     for idx, row in tqdm(test_data.iterrows(),total=test_data.shape[0]):
-        text.append('abc')
-        # img_path = ('/data/medg/misc/interpretable-report-gen/cache/images/' + str(row['dicom_id']) + '.png')
-        # if os.path.isfile(img_path):
-        #     seq, alphas = caption_image_beam_search(encoder, decoder, img_path, word_map, 5)
-        #     if seq != None and alphas != None:
-        #         alphas = torch.FloatTensor(alphas)
-        #         words = [rev_word_map[ind] for ind in seq]
-        #     else:
-        #         words = []
-        #     gen_text = ' '.join(words)
-        #     print(gen_text)
-        #     text.append(gen_text)
-        # else:
-        #     text.append('No image file.')
+        img_path = ('/data/medg/misc/interpretable-report-gen/cache/images/' + str(row['dicom_id']) + '.png')
+        if os.path.isfile(img_path):
+            seq, alphas = caption_image_beam_search(encoder, decoder, img_path, word_map, 5)
+            if seq != None and alphas != None:
+                alphas = torch.FloatTensor(alphas)
+                words = [rev_word_map[ind] for ind in seq]
+            else:
+                words = []
+            gen_text = ' '.join(words)
+            print(gen_text)
+            text.append(gen_text)
+        else:
+            text.append('No image file.')
     test_data['text'] = text
     gen_reports = test_data[['rad_id', 'text']]
-    gen_reports.to_csv('/data/medg/misc/liuguanx/gen-reports-5.tsv',sep='\t')
+    gen_reports.to_csv('/data/medg/misc/liuguanx/gen-reports-15.tsv',index=False,sep='\t')
